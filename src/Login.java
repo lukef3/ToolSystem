c5c1a51cc956e37584e78cbc0c7f4ee4c5324b8c
lukef3 <lukefoley@outlook.com>
2022-11-13 17:13:11 +0000

    project started

---
+++ src/Login.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+}
